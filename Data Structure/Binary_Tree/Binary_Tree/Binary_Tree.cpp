3d7e176aef834ca7f0c21eebebddc5d4fa7d89de
Huisu Hwang <gmltn1564@naver.com>
2020-02-14 21:33:04 +0900

    Binary_Tree_Implementation_Blue_Print How to Insert? Oh man...

--- Data Structure/Binary_Tree/Binary_Tree/Binary_Tree.cpp
+++ Data Structure/Binary_Tree/Binary_Tree/Binary_Tree.cpp
@@ -16,11 +16,13 @@ bool Tree_Node::External()
 Binary_Tree::Binary_Tree()
 {
 	root = NULL;
+	last = NULL;
 }
 
 Binary_Tree::Binary_Tree(Tree_Node* ptr)
 {
 	root = ptr;
+	last = ptr;
 }
 
 Tree_Node* Binary_Tree::get_root()
@@ -28,9 +30,33 @@ Tree_Node* Binary_Tree::get_root()
 	return root;
 }
 
+int Binary_Tree::get_height()
+{
+	int two_bass = 1;
+	for (int height = 0;; height++)
+	{
+		if (two_bass-1 >= size)
+		{
+			return height;
+		}
+		else
+		{
+			two_bass *= 2;
+		}
+	}
+}
+
 void Binary_Tree::print_data(Tree_Node* ptr)
 {
 	std::cout << "data : " << ptr->data << std::endl;
 }
 
+void Binary_Tree::Insert_Node(Tree_Node* ptr)
+{
+	if (root == NULL)
+	{
+		root = ptr;
+		last = ptr;
+	}
+}
 
--- Data Structure/Binary_Tree/Binary_Tree/Binary_Tree.h
+++ Data Structure/Binary_Tree/Binary_Tree/Binary_Tree.h
@@ -4,25 +4,40 @@
 class Tree_Node
 {
 	friend class Binary_Tree;
-public:
-	bool External();
-private :
+
+private:
 	Tree_Node* left;
 	Tree_Node* right;
+	Tree_Node* parent;
 	int data;
+
+public:
+	bool External();
+	friend std::istream& operator>>(std::istream& is, Tree_Node& ptr) {
+		is >> ptr.data;
+		return is;
+	}
+
 };
 
 class Binary_Tree {
+
+private:
+	Tree_Node* root;
+	Tree_Node* last;
+	int size;
+
 public:
 	Binary_Tree();
 	Binary_Tree(Tree_Node* ptr);
 	Tree_Node* get_root();
 	int get_height();
 	void print_data(Tree_Node* ptr);
+	void Insert_Node(Tree_Node* ptr);
+	void preorder(Tree_Node* ptr);
+	void inorder(Tree_Node* ptr);
+	void postorder(Tree_Node* ptr);
 
-
-private:
-	Tree_Node* root;
 };
 
 
